8f7b23cda2d4c7eafa6cc093c8e22f39226c5a6d
kevin <khan1782@gmail.com>
2017-02-15 15:48:49 -0600

    create friends virtual attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,16 @@ class User < ApplicationRecord
 
   has_many :user_games
   has_many :games, through: :user_games
+  has_many :initiated_friendships, class_name: "Friendship", foreign_key: :adder_id
+  has_many :received_friendships, class_name: "Friendship", foreign_key: :confirmer_id
 
-  has_many :friendships
-  has_many :friends, through: :friendships, source: :adder
-  has_many :inverse_friends, through: :friendships, source: :confirmer
+
+
+
+  def friends	
+  	initiated_friends = self.initiated_friendships.select{|friendship| friendship.confirmed?}.map{ |friendship| friendship.confirmer }
+  	received_friends = self.received_friendships.select{|friendship| friendship.confirmed?}.map{ |friendship| friendship.adder }
+  	initiated_friends + received_friends
+  end
+
 end
