1a3ce25ec802ed884dba81d9ee337e572b694ff0
kevin <khan1782@gmail.com>
2017-02-15 19:22:20 -0600

    got comments working for game

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,14 @@
 class CommentsController < ApplicationController
+	def create
+		new_comment = Comment.new(comment_params)
+		new_comment.game_id = params[:game_id]
+		new_comment.user_id = 1
+		new_comment.save
+		game = Game.find_by_id(new_comment.game_id)
+		redirect_to game_path(game)
+	end
+	private
+	def comment_params
+		params.require(:comment).permit(:body)
+	end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,8 @@
 class Game < ApplicationRecord
   has_many :user_games
   has_many :users, through: :user_games
-
+  has_many :comments
+  
   def self.bgg_top_ten
   	Game.all.sort_by {|game| game.bgg_rating.to_f}.reverse[0..9]
   end
