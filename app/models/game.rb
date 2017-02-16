87e730242faf4621dd46497ffc7970f530fea1bb
kevin <khan1782@gmail.com>
2017-02-16 10:41:44 -0600

    num player methods

--- app/models/game.rb
+++ app/models/game.rb
@@ -13,6 +13,14 @@ class Game < ApplicationRecord
 
   def self.user_top_ten
   	Game.all.sort_by {|game| game.user_games.count }.reverse[0..9]
+ 	end
+
+  def self.num_users(player_num)
+  	Game.all.select {|game| player_num >= game.min_players && player_num <= game.max_players }.sample(10)
+  end
+
+  def self.eight_plus
+  	Game.all.select {|game| game.max_players >= 8}
   end
 
 end
